a824a1e66b960d306d9722f7d72f33151bafe405
Hayden <49427552+m8rmclaren@users.noreply.github.com>
2021-04-28 00:24:51 -0700

    Change to Colton's main driver for heap insertion

--- C Files/main.c
+++ C Files/main.c
@@ -5,118 +5,159 @@
 
 typedef char Datatype_h; // Typedef char for datatype, demonstrate extensibility of C
 
-typedef struct hNode {
-    struct hNode *leftNode, *rightNode;
-    Datatype_h* word;
+typedef struct hNode
+{
+    Datatype_h *word;
     int size;
-} HN;
+}	HN;
 
-void insertHeap(HN **heap, HN *newNode);
-HN *removeHeap(HN **heap);
-HN *getNewNode(Datatype_h* word, int size);
+typedef struct littleEndUpHeap{
+    HN *theHeap; // heap array to store HN data
+    int count; // current size of HN 
+}	HEAP;
 
-HN *readFile(const char* filename);
+HEAP *constructHeapFromFP(FILE *fp);
+void deconstructHeap(HEAP *heap);
 
+HN *getNewNode(Datatype_h *word, int size);
+HEAP *createHeap();
+void insertNode(HEAP *h, HN *newNode);
+void bubbleUp(HEAP *h, int theIndex);
+void siftDown(HEAP *h, int parent);
+HN *removeNode(HEAP *h);
+
+#define MAXSIZE 10000
+#define FILE_PATH "/Users/haydenroszell/Documents/School/College/ERAU/Sophomore/sem2/CS332/FinalProject/C_Heap/text.txt"
 int main() {
-    readFile("/Users/haydenroszell/Documents/School/College/ERAU/Sophomore/sem2/CS332/FinalProject/C_Heap/text.txt");
+    FILE* fp = fopen(FILE_PATH, "r");
+    if (fp == NULL) {
+        perror(FILE_PATH);
+        return 1;
+    }
+    HEAP* heap = constructHeapFromFP(fp);
+    fputc('\n', stdout);
+    fclose(fp);
+    deconstructHeap(heap);
     return 0;
 }
 
-HN *readFile(const char* filename) {
-    Datatype_h letter[1024];
-    FILE* fp = fopen(filename, "r");
-    if (fp == NULL) {
-        perror(filename);
-        return NULL;
+void deconstructHeap(HEAP *heap) {
+    while (heap->count > 1) {
+        HN* temp = removeNode(heap);
+        printf("%d: %s\n", temp->size, temp->word);
     }
+}
+
+HEAP *constructHeapFromFP(FILE *fp) {
+    Datatype_h letter[1024];
+    HEAP* heap = createHeap();
 
     while (fscanf(fp, " %1023s", letter) == 1) {
-        int i = 0, j = 0, len = 0;
-        for (i = 0, j = 0, len = (int)strlen(letter); i < len; i++) {
+        int i, j, len;
+        for (i = 0, j = 0, len = (int) strlen(letter); i < len; i++) {
             if (!ispunct(letter[i])) {
                 letter[j] = tolower(letter[i]);
                 j++;
             }
         }
         letter[j] = '\0';
-        puts(letter);
-    }
-    return NULL;
-}
-
-
-void insertHeap(HN **heap, HN *newNode) {
-    HN *node = newNode;
-    ++heap[0]->size;
-    int count = heap[0]->size;
-    int search = 1;
-    heap[count] = node;
-
-    while (search == 1) {
-        if (count % 2 == 0) {
-            if (heap[count/2]->size > node->size && count/2 > 0) {
-                HN *temp = heap[count / 2];
-                heap[count / 2] = heap[count];
-                heap[count] = temp;
-                count /= 2;
-            } else
-                search = 0;
-        } else {
-            if (heap[(count-1)/2]->size > node->size && (count-1)/2 > 0) {
-                HN *temp = heap[(count - 1) / 2];
-                heap[(count-1)/2] = heap[count];
-                heap[count] = temp;
-                count -= 1;
-                count /= 2;
-            } else
-                search = 0;
+        if (j > 0) {
+            insertNode(heap, getNewNode(letter, j));
         }
     }
+    return heap;
 }
 
-HN *removeHeap(HN **heap) {
-    int index = 1;
-    int search = 1;
+/***********************************************************************************/
+HN *getNewNode(Datatype_h *word, int size)
+{
+    HN *temp = (HN*)malloc(sizeof(HN));
 
-    if (heap[0]->size < 1) {
-        printf("Heap is empty!\n");
-        return heap[0];
-    }
+    temp->word = strcpy(malloc(size), word); // Assign key of new node to user's input
+    temp->size = size; // Assign data of new node to user's input
 
-    HN *returnVal = heap[1];
-
-    heap[1] = heap[heap[0]->size];
-    --heap[0]->size;
-
-    while (search == 1 && heap[0]->size > 0) {
-        if (index*2 > heap[0]->size)
-            search = 0;
-        else if (heap[index * 2]->size < heap[(index * 2) + 1]->size && heap[index * 2]->size < heap[index]->size && index*2 < heap[0]->size) {
-            HN *temp = heap[index * 2];
-            heap[index * 2] = heap[index];
-            heap[index] = temp;
-            index *= 2;
-
-        } else if (heap[index * 2]->size > heap[(index * 2) + 1]->size && heap[(index * 2) + 1]->size < heap[index]->size && index*2 < heap[0]->size) {
-            HN *temp = heap[(index * 2) + 1];
-            heap[(index * 2) + 1] = heap[index];
-            heap[index] = temp;
-            index *= 2;
-            index += 1;
-        } else {
-            heap[heap[0]->size+1] = NULL;
-            return returnVal;
-        }
-    }
-    heap[heap[0]->size+1] = NULL;
-    return returnVal;
+    return temp;
 }
+/***********************************************************************************/
+HEAP *createHeap()
+{
+    HEAP *h = (HEAP*)malloc(sizeof(HEAP));
 
-HN *getNewNode(Datatype_h* word, int size) {
-    HN *new = (HN*)malloc(sizeof(HN));
-    new->leftNode = new->rightNode = NULL;
-    new->word = word;
-    new->size = size;
+    h->count = 1; // initialize heap's array index to 1 (not 0!)
+    h->theHeap = (HN*)malloc(MAXSIZE*sizeof(HN));
 
-    return new;
+    return h;
+}
+/***********************************************************************************/
+void insertNode(HEAP *h, HN *newNode)
+{
+    if (h->count < MAXSIZE)
+    {
+        h->theHeap[h->count] = *newNode; // insert new node into heap array
+        bubbleUp(h, h->count); // sort until heap properties are met
+        h->count++; // increment size of heap for indexing
+    }
+}
+/***********************************************************************************/
+void bubbleUp(HEAP *h, int theIndex)
+{
+    HN tempNode;
+    int parent = (theIndex) / 2; // as per algorithm on slide 9
+
+    if (h->theHeap[parent].size > h->theHeap[theIndex].size)
+    {
+        tempNode = h->theHeap[parent]; // set temp to current parent
+        h->theHeap[parent] = h->theHeap[theIndex]; // new parent is now index from call
+        h->theHeap[theIndex] = tempNode; // index from call is now old parent
+
+        bubbleUp(h, parent); // recursive call until parent is minimum
+    }
+}
+/***********************************************************************************/
+void siftDown(HEAP *h, int parent)
+{
+    HN tempNode;
+    int leftIndex = parent * 2; // left child of parent
+    int rightIndex = parent * 2 + 1; // right child of parent
+    int smallest; // smallest value of heap
+
+    if ((leftIndex < 1) || (leftIndex >= h->count)) // if there is no left child
+        leftIndex = 0;
+    if ((rightIndex < 1) || (rightIndex >= h->count)) // if there is no right child
+        rightIndex = 0;
+
+    if ((leftIndex != 0) && (h->theHeap[parent].size > h->theHeap[leftIndex].size)) // if left child is less than parent
+        smallest = leftIndex; // left child is minimum
+    else
+        smallest = parent; // otherwise, parent is already minimum
+
+    if ((rightIndex != 0) && (h->theHeap[smallest].size > h->theHeap[rightIndex].size)) // if right child is less than parent
+        smallest = rightIndex; // right child is minimum
+
+    if (parent != smallest) // if parent isn't already minimum
+    {
+        tempNode = h->theHeap[smallest]; // set temp node to the minimum node as determined by above indexing
+        h->theHeap[smallest] = h->theHeap[parent]; // set min node to parent node
+        h->theHeap[parent] = tempNode; // set the parent to tempNode
+
+        siftDown(h, smallest); // recursive call until parent is minimum
+    }
+}
+/***********************************************************************************/
+HN *removeNode(HEAP *h)
+{
+    HN *removedNode = (HN*)malloc(sizeof(HN));
+
+    if (h->count == 1) // simple error-checking to see if heap has any nodes remaining before removal
+    {
+        printf("\nCannot remove - heap is empty\n");
+        return removedNode;
+    }
+    *removedNode = h->theHeap[1]; // store removed node for return
+    h->theHeap[1] = h->theHeap[h->count-1]; // decrement index of heap
+    h->count--; // decrement size of heap
+
+    siftDown(h, 1); // recursive call until heap properties are met
+
+    return removedNode;
 }
