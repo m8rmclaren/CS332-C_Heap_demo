87ffe71dd6a3cfe0319000dece92d1ed91c9c27f
Colton Van Orsdel <colton@vanorsdel.us>
2021-04-27 22:15:08 -0700

    Initial Ada progress upload
    See readme and code comments for relevant information.

---
+++ ada_heapsort.adb
@@ -0,0 +1,28 @@
+-----------------------------------------------------------------
+-- Author: Colton Van Orsdel
+-- File: ada_heapsort.adb
+-- Purpose: Similar to a function's ".c" file in C, to consolidate
+-- 			the primary working function code for use in our main
+-----------------------------------------------------------------
+with Ada.Text_IO, Ada.Integer_Text_IO; -- not yet sure what these include, got them from Dr. Van Hilst's videos
+
+package body Ada_Heapsort is
+
+-- this is where our full definitions of the procedures (functions) defined from ada_heapsort.ads will go
+	procedure Insert_Heap(Heap: Heap_Node_Type'; New_Node: Heap_Node_Type') is
+	begin 
+
+	end Insert_Heap;
+
+	procedure Remove_Heap(Heap: Heap_Node_Type') is
+	begin
+
+	end Remove_Heap;
+
+	procedure Get_New_Node(Data_Element: Data_Type'; Data_Element_Size: Integer) is
+
+	begin
+
+	end Get_New_Node;
+
+end Ada_Heapsort;
