7cf790a3e3bd68a992b19aa413fa324fe4803415
Colton Van Orsdel <colton@vanorsdel.us>
2021-05-01 17:19:50 -0700

    Added Arseniy's timing solution

--- CProgram/main.c
+++ CProgram/main.c
@@ -1,13 +1,19 @@
 /**********************************************************************
-// Authors: Hayden Roszell, Colton Van Orsdel
+// Authors: Hayden Roszell, Colton Van Orsdel, Arseniy Gorbanev
 // File:    main.c
 // Purpose: Calls all functions to perform the heapsort operation
 **********************************************************************/
 #include "c_heapsort.h"
+#include <stdio.h>
+#include <sys/time.h>
 
-// Compile using: gcc -o c_heapsort main.c c_heapsort.c -I.
+// Compile using: gcc -std=c11 -o c_heapsort main.c c_heapsort.c -I.
 
 int main() {
+    // timing start
+    struct timeval startTime, endTime;
+    gettimeofday(&startTime, 0);
+    
     FILE* fp = fopen(FILE_PATH, "r");
     if (fp == NULL) {
         perror(FILE_PATH);
@@ -17,5 +23,15 @@ int main() {
     fputc('\n', stdout);
     fclose(fp);
     deconstructHeap(heap);
+    
+    // timing calculation
+    gettimeofday(&endTime, 0);
+    long seconds = endTime.tv_sec - startTime.tv_sec;
+    long microseconds = endTime.tv_usec - startTime.tv_usec;
+    double elapsed = seconds + microseconds*1e-6;
+    
+    printf("Runtime = %li microseconds.\n", microseconds);
+    printf("Runtime = %f seconds.\n", elapsed);
+    
     return 0;
 }
